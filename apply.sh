1fea0a437d0d79c156cb3fae0fd6a7baa74256d3
Rasmus <pez@pez.sh>
2022-12-20 11:57:45 +0000

    enabled federation of istio-operator-controller and CRDs

--- apply.sh
+++ apply.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for file in `ls **/*.yaml | grep -vE "^virtualservice-host-mutator"`
+for file in `ls **/*.yaml | grep -vE "^(virtualservice-host-mutator|k3s)"`
 do
     kubectl apply -f $file
 done
